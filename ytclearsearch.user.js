2d431d1c047ce62fb2d9e580e8c005decc45762e
Jones <53267697+Katzenwerfer@users.noreply.github.com>
2024-03-09 20:12:00 +0000

    New script

---
+++ ytclearsearch.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        YouTube - Search Results Cleaner
+// @version     1
+// @author      Jones
+// @match       *://www.youtube.com/results?search_query
+// @grant       GM_addStyle
+// ==/UserScript==
+
+// This script is based on this userstyle by lednerg
+// https://greasyfork.org/en/scripts/454513-youtube-search-results-cleaner
+// Originally licensed CC-BY-NC-SA-4.0
+
+GM_addStyle(``)
